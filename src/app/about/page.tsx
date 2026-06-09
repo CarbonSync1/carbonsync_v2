03fcd53b7aa122e26c84f3d3505b662e8a07bf26
Abhijeet Rao <abhijeetrao7800@gmail.com>
2026-06-10 00:00:03 +0530

    Remove Experience and Logos components from AboutPage; streamline content presentationit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,11 +6,10 @@ const Innovation = dynamic(() => import('@/components/about-us/Innovation'));
 const Offerings = dynamic(() => import('@/components/about-us/Offerings'));
 const Team = dynamic(() => import('@/components/about-us/Team'));
 const Newsroom = dynamic(() => import('@/components/about-us/Newsroom'));
-const Logos = dynamic(() => import('@/components/about-us/Logos'));
+
 const Timeline = dynamic(() => import('@/components/about-us/Timeline'));
 const Mentors = dynamic(() => import('@/components/about-us/Mentors'));
-const CallToAction = dynamic(() => import('@/components/about-us/CallToAction'));
-const Experience = dynamic(() => import('@/components/about-us/Experience'));
+
 
 import '@/components/about-us/about-styles.css';
 
@@ -23,11 +22,8 @@ export default function AboutPage() {
       <Offerings />
       <Team />
       <Newsroom />
-      <Logos />
       <Timeline />
       <Mentors />
-      <CallToAction />
-      <Experience />
     </>
   );
 }
